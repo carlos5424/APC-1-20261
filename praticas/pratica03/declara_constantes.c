16929f528007d8960862bb8e7ed712c2d500a2aa
unknown <2612082019@ACADEMICO.IESB.BR>
2026-04-08 10:16:22 -0300

    feat: conclui pratica03. Fecha #3

---
+++ praticas/pratica03/declara_constantes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define PI 3.14159265
+
+int main () {
+
+    const double E = 2.71828182;
+
+    printf("Esse e o valor de pi: %.8f\n", PI);
+
+    printf("Esse e o valor de Euler: %.8f\n", E);
+
+    return 0;
+}
